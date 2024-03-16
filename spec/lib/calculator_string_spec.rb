166fe01f6b8fe757a21bc478624fdc560dabc36d
vinod-verma <vi.vermad@gmail.com>
2024-03-16 11:33:24 +0530

    test: expect custom delimiter in string and return of numbers seperated by commas by ignoring custom delimiter

--- spec/lib/calculator_string_spec.rb
+++ spec/lib/calculator_string_spec.rb
@@ -37,5 +37,11 @@ RSpec.describe CalculatorString do
         expect(subject.add("1\n2,3")).to eq(6)
       end
     end
+
+    context "when given a custom delimiter" do
+      it "returns the sum of numbers using the custom delimiter" do
+        expect(subject.add("//;\n1;2")).to eq(3)
+      end
+    end
   end
 end
