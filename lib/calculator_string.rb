8b24bb9b3d5312c77ee3e82e48aee4999dc38ca6
vinod-verma <vi.vermad@gmail.com>
2024-03-16 11:48:46 +0530

    feature: return sum of numbers seperated by commas by ignoring customer delimiter

--- lib/calculator_string.rb
+++ lib/calculator_string.rb
@@ -2,6 +2,13 @@ class CalculatorString
   def add(numbers)
     return 0 if numbers.empty?
 
-    numbers.split(/\n|,/).sum(&:to_i)
+    delimiter = ","
+    custom_delimiter = numbers.match(/^\/\/(.+)\n/)
+    if custom_delimiter
+      delimiter = custom_delimiter[1][1..-2]
+      numbers = numbers.sub(/^\/\/(.+)\n/, "")
+    end
+
+    numbers.split(/\n|#{delimiter}/).sum(&:to_i)
   end
 end
