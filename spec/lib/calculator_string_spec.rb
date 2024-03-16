cd768df3f34c5972d5c853bca9b9df06fd100944
vinod-verma <vi.vermad@gmail.com>
2024-03-16 11:00:29 +0530

    renamed spec file

---
+++ spec/lib/calculator_string_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe CalculatorString do
+  describe "#add" do
+    context "when given an empty string" do
+      it "returns 0" do
+        expect(subject.add("")).to eq(0)
+      end
+    end
+  end
+end
